8573c70d88aedad4f54c2e066e80f25943380810
sevas <f.degroef@gmail.com>
2010-12-05 20:12:33 +0100

    refactored importCalendar.py in CourseCalendar & utils  modules

---
+++ src/utils.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+def convert_time(s):
+    '''convert string time into datetime struct'''
+    d = datetime.strptime(s,"%H:%M")
+    return d
+
+
+def split_weeks(weeks):
+    '''split string containing weeks info into separated fields
+    e.g. "1,5-7"  ---> [1,5,6,7]'''
+    s = weeks.split(',')
+    w = []
+    for f in s:
+        sf = f.split('-')
+        if len(sf)>1:
+            w.extend(range(int(sf[0]),int(sf[1])+1))
+        else:
+            w.append(int(f))
+    return w
+
