250cff58163c5062ce6a20a4eb98cc60cf70d838
sevas <f.degroef@gmail.com>
2010-12-05 20:19:02 +0100

    extract CourseCalendar usage to examples/

---
+++ examples/test_course_calendar.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append('../src')
+
+from CourseCalendar import CourseCalendar
+
+
+if __name__=="__main__":
+    from pprint import pprint
+    calendar = CourseCalendar("http://164.15.72.157:8080", "INFOH500")
+    calendar.load_events()
+    pprint(calendar.get_events())
--- src/CourseCalendar.py
+++ src/CourseCalendar.py
@@ -122,9 +122,3 @@ class CourseCalendar(object):
         return event_list
 
 
-
-if __name__=="__main__":
-    from pprint import pprint
-    calendar = CourseCalendar("http://164.15.72.157:8080", "INFOH500")
-    calendar.load_events()
-    pprint(calendar.get_events())
