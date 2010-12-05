3abe78dc4eb48c012e43d7c27de9eaa57534c138
sevas <f.degroef@gmail.com>
2010-12-05 20:11:09 +0100

    splitted sample cases

--- src/example.py
+++ src/example.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+
 import argparse
-from importCalendar import get_html, parse_header, parse_table
-    
+
+from importCalendar import get_html, get_html_by_url, parse_header, parse_table
+from csvwriter import export_csv
+
 def process(mnemo,host,first_monday,dest_filename):    
     html = get_html(host,mnemo)    
     head = parse_header(html)
@@ -15,11 +19,16 @@ def process_by_url(url,first_monday,dest_filename):
     export_csv(head, events, dest_filename,first_monday)
 
 
-def test():
+def test_all():
+    test_batch()
+    test_with_url()
+
+
+def test_batch():
     '''test import function for a mnemonic list'''
     print 'import calendar test --> csv files'
     mnemo = ['INFOH500','BIMEH404','STATH400']
-    host = 'http://164.15.72.157'
+    host = 'http://164.15.72.157:8080'
     first_monday = '20/09/2010'
     
     for m in mnemo:
@@ -27,10 +36,14 @@ def test():
         process(m, host, first_monday, dest_filename)
         print dest_filename
 
+
+def test_with_url():
     #test by url
-    url = '%s/Reporting/Individual;Student%20Set%20Groups;id;%23SPLUSA629A0?&template=Ann%E9e%20d%27%E9tude&weeks=1-14&days=1-6&periods=5-33&width=0&height=0' % host
+    first_monday = '20/09/2010'
+    url = 'http://164.15.72.157:8080/Reporting/Individual;Student%20Set%20Groups;id;%23SPLUSA629A0?&template=Ann%E9e%20d%27%E9tude&weeks=1-14&days=1-6&periods=5-33&width=0&height=0'
     dest_filename = 'import_by_url.csv'
     process_by_url(url, first_monday, dest_filename)
+
 
 
     
@@ -51,15 +64,33 @@ if __name__ == '__main__':
     parser.add_argument('-d', required=False,
                         help='Monday date in the week 1 [20/09/2010]',
                         default = '20/09/2010')
-    
+
+
     parser.add_argument('-t','--test', required=False,
                         action='store_true',
                         help='test the program on some courses',
                         default = False)
-    
+
+
+    parser.add_argument('-b','--test_batch', required=False,
+                        action='store_true',
+                        help='test the program on a batch of courses',
+                        default = False)
+
+    parser.add_argument('-u','--test_url', required=False,
+                        action='store_true',
+                        help='test the program on a course by supplying a full url',
+                        default = False)
+
+
+
     args = parser.parse_args()
     if args.test:
-        test()
+        test_all()
+    elif args.test_batch:
+        test_batch()
+    elif args.test_url:
+        test_with_url()
     else:
         if args.mnemo is None:
             parser.print_help()
