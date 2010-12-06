e6dd5110ec3c6588b33a27061e220efaa06d56cb
odebeir <devnull@localhost>
2010-12-06 16:42:38 +0100

    add ical writer module, ical module returns a string date and time are not yet effective...

---
+++ examples/test_ical_writer.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append("../src")
+
+from gehol.coursecalendar import CourseCalendar
+from gehol.icalwriter import export_ical
+
+
+if __name__=="__main__":
+    print 'import calendar test --> ical string'
+    all_courses = ['INFOH500','BIMEH404','STATH400']
+    host = 'http://164.15.72.157:8080'
+    first_monday = '20/09/2010'
+
+    for course in all_courses:
+        dest_filename = 'agenda_%s.csv' % course
+        print "Saving %s events to %s" % (course, dest_filename)
+        cal = CourseCalendar(host, course)
+        cal.load_events()
+        ical_string = export_ical(cal.metadata, cal.events, dest_filename, first_monday)
+        print ical_string
+
+
+
---
+++ src/gehol/icalwriter.py
@@ -0,0 +1,48 @@
+from datetime import datetime, timedelta
+from icalendar import Calendar, Event, vText
+
+
+
+def export_ical(head,events,filename,first_monday):
+    '''export events into iCal format
+    the file is returned as a string
+    first_monday corresponds to the monday date of week 1 in Gehol
+    '''
+    date_init = datetime.strptime(first_monday,'%d/%m/%Y')
+
+    cal = Calendar()
+    cal.add('prodid', 'https://bitbucket.org/odebeir/ulbcalendar2cvs')
+    cal.add('version', '2.0')
+
+    for event in events:
+        n = len(event['weeks'])
+        for (i,sub) in enumerate(event['weeks']):
+            summary = '%s%s (%d/%d)'%(head['mnemo'],event['type'],i+1,n)
+            #add offset corresponding to week numbers for each event repetition
+            delta = timedelta(days=(sub-1)*7+(event['day']))
+            dtstart = date_init+delta
+#            start_date = (date_init+delta).strftime("%d/%m/%y")
+#            start_time = event['start'].strftime("%I:%M:%S %p")
+#            end_date = start_date
+#            end_time = event['end'].strftime("%I:%M:%S %p")
+#            all_day_event = 'False'
+            organizer = head['title']+' titulaire : '+ head['tutor']
+
+            location = event['location']
+#            private = 'False'
+
+            cal_event = Event()
+
+            cal_event.add('summary', summary)
+            cal_event.add('dtstart', dtstart)
+            cal_event['location'] = vText(location)
+            cal_event['organizer'] = vText(organizer)
+
+    #        cal_event.add('dtend', datetime(2005,4,4,10,0,0,tzinfo=UTC))
+    #        cal_event.add('dtstamp', datetime(2005,4,4,0,10,0,tzinfo=UTC))
+    #        event['uid'] = '20050115T101010/27346262376@mxm.dk'
+    #        event.add('priority', 5)
+
+            cal.add_component(cal_event)
+
+    return cal.as_string()
