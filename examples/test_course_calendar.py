7a79df217f63421ca52f659d1ec1bd0cd6f0fc13
odebeir <devnull@localhost>
2010-12-15 15:34:09 +0100

    delete old example, fix host in test_course_calendar

--- examples/test_course_calendar.py
+++ examples/test_course_calendar.py
@@ -6,7 +6,7 @@ from gehol.coursecalendar import CourseCalendar
 
 if __name__=="__main__":
     from pprint import pprint
-    calendar = CourseCalendar("http://164.15.72.157:8080", "INFOH500")
+    calendar = CourseCalendar("164.15.72.157:8080", "INFOH500")
     print "getting events from : %s" % calendar.url
     calendar.load_events()
     pprint(calendar.events)
