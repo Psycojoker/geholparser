314d311e1377e533fd43ad1003e061621001757f
sevas <f.degroef@gmail.com>
2010-12-24 15:53:25 +0100

    first rfc5545 ical exporter

--- examples/test_student_calendar.py
+++ examples/test_student_calendar.py
@@ -2,25 +2,31 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Frederic'
 
+import os
 import sys
 sys.path.append("../src")
 from gehol.studentcalendar import StudentCalendar
-from gehol.converters.icalwriter import convert_student_calendar_to_ical, write_ical_to_file
+from gehol.converters.icalwriter import convert_student_calendar_to_ical, write_ical_to_file, convert_student_calendar_to_ics_rfc5545
 from pprint import pprint
 
+DATA_DIR = "../data/student/"
 DATA_FILE = "../data/student/IR_MA1_img_1_14.html"
 DATA_FILE = "../data/student/SC_INFO_MA1_multimedia_1_14.html"
 DATA_FILE = "../data/student/PHILO_BA1_HIST_1_14.html"
 
-if __name__ == "__main__":
-    f = open(DATA_FILE)
+
+def make_ical(filename):
+    f = open(filename)
     cal = StudentCalendar(f)
     print cal.header_data
     pprint(cal.events)
 
     first_monday = '20/09/2010'
-    ical_content = convert_student_calendar_to_ical(cal, first_monday)
-    #print ical_content
+    ical_content = convert_student_calendar_to_ics_rfc5545(cal, first_monday)
     write_ical_to_file(ical_content, "%s.ics" % cal.description)
 
+
+if __name__ == "__main__":
+    for f in os.listdir(DATA_DIR):
+        make_ical(DATA_DIR + f)
 
--- src/gehol/converters/icalwriter.py
+++ src/gehol/converters/icalwriter.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 from icalendar import Calendar, Event, vText
-
+from StringIO import StringIO
 
 
 def to_ical(head,events,first_monday):
@@ -50,6 +50,58 @@ def to_ical(head,events,first_monday):
 
 
 
+class IcalWriter(object):
+    """
+    Simple ical writer, compatible with rfc 5545 (although not extensively)
+    """
+
+    provider = 'https://bitbucket.org/odebeir/geholimport'
+    version = '2.0'
+
+    def __init__(self):
+        self.name = ""
+        self.description = ""
+        self.summary = ""
+        self.start_date = ""
+        self.events = []
+
+
+
+    def to_string(self):
+        out = StringIO()
+        out.write("BEGIN:VCALENDAR")
+        out.write("VERSION:%s" % self.version)
+        out.write("PRODID:%s" % self.provider)
+
+        out.write("X-WR-CALNAME:%s" % self.name)
+        out.write("X-WR-CALDESC:%s" % self.description)
+
+
+        out.write("BEGIN:VTIMEZONE")
+        out.write("TZID:Europe/Brussels")
+        out.write("BEGIN:STANDARD")
+        out.write("TZOFFSETFROM:+0100")
+        out.write("TZOFFSETTO:+0100")
+        out.write("DTSTART:%s" % self.start_date)
+        out.write("END:STANDARD")
+        out.write("END:VTIMEZONE")
+
+
+        for event in self.events:
+            out.write("BEGIN:VEVENT")
+            out.write("DTSTAMP:20101229T213000")
+            out.write("DTSTART;TZID=Europe/Brussels:20101229T213000")
+            out.write("DTEND;TZID=Europe/Brussels:20101229T235900")
+            out.write("SUMMARY:%s")
+            out.write("DESCRIPTION:%s")
+            out.write("LOCATION:%s" % event['location'])
+            #out.write("GEO:5.092867;51.557655")
+            out.write("END:VEVENT")
+
+        out.write("END:VCALENDAR")
+
+
+
 def convert_student_calendar_to_ical(student_calendar, first_monday):
     date_init = datetime.strptime(first_monday,'%d/%m/%Y')
 
@@ -81,14 +133,68 @@ def convert_student_calendar_to_ical(student_calendar, first_monday):
 
             ical_event.add('summary', event_summary)
             ical_event.add('location', event_location)
-            ical_event.add('cn', "foo")#event_organizer.encode('iso-8859-2'))
             ical_event.add('dtstart', dtstart)
             ical_event.add('dtend', dtend)
+            ical_event.add('description', event_organizer)
 
             ical.add_component(ical_event)
 
     return ical.as_string()
 
+
+
+def convert_student_calendar_to_ics_rfc5545(student_calendar, first_monday):
+    date_init = datetime.strptime(first_monday,'%d/%m/%Y')
+
+    def write_line(out, line):
+        out.write(line+'\r\n')
+
+
+    out = StringIO()
+    write_line(out, "BEGIN:VCALENDAR")
+    write_line(out, "VERSION:%s" % "2.0")
+    write_line(out, "PRODID:%s" % 'https://bitbucket.org/odebeir/geholimport')
+
+    write_line(out, "X-WR-CALNAME:%s" % student_calendar.profile)
+    write_line(out, "X-WR-CALDESC:%s" % student_calendar.description)
+
+    write_line(out, "BEGIN:VTIMEZONE")
+    write_line(out, "TZID:Europe/Brussels")
+    write_line(out, "BEGIN:STANDARD")
+    write_line(out, "TZOFFSETFROM:+0100")
+    write_line(out, "TZOFFSETTO:+0100")
+    write_line(out, "DTSTART:%s" % date_init.strftime("%Y%m%dT%H%M%S"))
+    write_line(out, "END:STANDARD")
+    write_line(out, "END:VTIMEZONE")
+
+
+    for event in student_calendar.events:
+        event_summary =  "%s (%s)" % (event['title'], event['type'])
+        event_organizer = event['organizer']
+        event_location = event['location']
+        event_description = "%s [%s]" % (event_summary, event_organizer)
+
+        for (i, event_week) in enumerate(event['weeks']):
+            delta = timedelta(days=(event_week-1)*7+(event['day']))
+            dtstart = date_init+delta + timedelta(hours = event['start_time'].hour,
+                                                    minutes = event['start_time'].minute)
+            dtend = date_init+delta + timedelta(hours = event['stop_time'].hour,
+                                            minutes = event['stop_time'].minute)
+
+            write_line(out, "BEGIN:VEVENT")
+            write_line(out, "DTSTAMP:%s" %  dtstart.strftime("%Y%m%dT%H%M%S"))
+            write_line(out, "DTSTART;TZID=Europe/Brussels:%s" % dtstart.strftime("%Y%m%dT%H%M%S"))
+            write_line(out, "DTEND;TZID=Europe/Brussels:%s" % dtend.strftime("%Y%m%dT%H%M%S"))
+            write_line(out, "SUMMARY:%s" % event_summary)
+            write_line(out, "DESCRIPTION:%s" % event_description)
+            write_line(out, "LOCATION:%s" % event_location)
+            #write_line(out, "GEO:5.092867;51.557655")
+            write_line(out, "END:VEVENT")
+
+    write_line(out, "END:VCALENDAR")
+    ical_string = out.getvalue()
+    out.close()
+    return ical_string.encode("iso-8859-2")
 
 
 def export_ical(head,events,dest_filename, first_monday):
