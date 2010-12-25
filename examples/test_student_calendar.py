dab47fda629141f9f969681e06a63ab5b590fba0
sevas <f.degroef@gmail.com>
2010-12-25 20:57:38 +0100

    refactored the new ics writer and the file writing utility func

--- examples/test_student_calendar.py
+++ examples/test_student_calendar.py
@@ -7,7 +7,8 @@ import sys
 sys.path.append("../src")
 from gehol import GeholProxy
 from gehol.studentcalendar import StudentCalendar
-from gehol.converters.icalwriter import  write_ical_to_file, convert_calendar_to_ics_rfc5545
+from gehol.converters.utils import  write_content_to_file
+from gehol.converters.rfc5545icalwriter import convert_calendar_to_ical
 from pprint import pprint
 
 DATA_DIR = "../data/student/"
@@ -23,8 +24,8 @@ def make_ical_from_local_file(filename):
     print cal.header_data
     pprint(cal.events)
 
-    ical_content = convert_student_calendar_to_ics_rfc5545(cal, first_monday)
-    write_ical_to_file(ical_content, "%s.ics" % cal.description)
+    ical_content = convert_calendar_to_ical(cal, first_monday)
+    write_content_to_file(ical_content, "%s.ics" % cal.description)
 
 
 URLs = [("MA1 en sciences informatiques - Spécialisée - Multimedia", "http://164.15.72.157:8080/Reporting/Individual;Student%20Set%20Groups;id;%23SPLUS0FACD0?&template=Ann%E9e%20d%27%E9tude&weeks=1-14&days=1-6&periods=5-33&width=0&height=0"),
@@ -36,10 +37,10 @@ URLs = [("MA1 en sciences informatiques - Spécialisée - Multimedia", "http://1
 def make_ical_from_url(name, url):
     gehol_proxy = GeholProxy()
     cal = gehol_proxy.get_student_calendar_from_url(url)
-    ical_data = convert_calendar_to_ics_rfc5545(cal, first_monday)
+    ical_data = convert_calendar_to_ical(cal, first_monday)
     outfile = "%s.ics" % name
     print "writing ical file : %s" % outfile
-    write_ical_to_file(ical_data, outfile)
+    write_content_to_file(ical_data, outfile)
 
 
 if __name__ == "__main__":
--- src/gehol/converters/icalwriter.py
+++ src/gehol/converters/icalwriter.py
@@ -1,13 +1,11 @@
 from datetime import datetime, timedelta
 from icalendar import Calendar, Event, vText
-from StringIO import StringIO
-
-
+from utils import write_content_to_file
 
 def to_ical(head,events,first_monday):
     '''export events into iCal format
     the file is returned as a string
-    first_monday corresponds to the monday date of week 1 in Gehol
+    first_monday is the date of the monday in week 1 in Gehol (in the DD/MM/YYYY format)
     '''
     date_init = datetime.strptime(first_monday,'%d/%m/%Y')
 
@@ -45,66 +43,6 @@ def to_ical(head,events,first_monday):
     return cal.as_string()
 
 
-def convert_calendar_to_ics_rfc5545(calendar, first_monday):
-    date_init = datetime.strptime(first_monday,'%d/%m/%Y')
-
-    def write_line(out, line):
-        out.write(line+'\r\n')
-
-    out = StringIO()
-    write_line(out, "BEGIN:VCALENDAR")
-    write_line(out, "VERSION:%s" % "2.0")
-    write_line(out, "PRODID:%s" % 'https://bitbucket.org/odebeir/geholimport')
-
-    write_line(out, "X-WR-CALNAME:%s" % calendar.name)
-    write_line(out, "X-WR-CALDESC:%s" % calendar.description)
-
-    write_line(out, "BEGIN:VTIMEZONE")
-    write_line(out, "TZID:Europe/Brussels")
-    write_line(out, "BEGIN:STANDARD")
-    write_line(out, "TZOFFSETFROM:+0100")
-    write_line(out, "TZOFFSETTO:+0100")
-    write_line(out, "DTSTART:%s" % date_init.strftime("%Y%m%dT%H%M%S"))
-    write_line(out, "END:STANDARD")
-    write_line(out, "END:VTIMEZONE")
-
-
-    for event in calendar.events:
-        event_summary =  "%s (%s)" % (event['title'], event['type'])
-        event_organizer = event['organizer']
-        event_location = event['location']
-        event_description = "%s [%s]" % (event_summary, event_organizer)
-
-        for (i, event_week) in enumerate(event['weeks']):
-            delta = timedelta(days=(event_week-1)*7+(event['day']))
-            dtstart = date_init+delta + timedelta(hours = event['start_time'].hour,
-                                                    minutes = event['start_time'].minute)
-            dtend = date_init+delta + timedelta(hours = event['stop_time'].hour,
-                                            minutes = event['stop_time'].minute)
-
-            write_line(out, "BEGIN:VEVENT")
-            write_line(out, "DTSTAMP:%s" %  dtstart.strftime("%Y%m%dT%H%M%S"))
-            write_line(out, "DTSTART;TZID=Europe/Brussels:%s" % dtstart.strftime("%Y%m%dT%H%M%S"))
-            write_line(out, "DTEND;TZID=Europe/Brussels:%s" % dtend.strftime("%Y%m%dT%H%M%S"))
-            write_line(out, "SUMMARY:%s" % event_summary)
-            write_line(out, "DESCRIPTION:%s" % event_description)
-            write_line(out, "LOCATION:%s" % event_location)
-            #write_line(out, "GEO:5.092867;51.557655")
-            write_line(out, "END:VEVENT")
-
-    write_line(out, "END:VCALENDAR")
-    ical_string = out.getvalue()
-    out.close()
-
-    return ical_string.encode('utf-8')
-
-
 def export_ical(head,events,dest_filename, first_monday):
     ical_string = to_ical(head,events,first_monday)
-    fd = open(dest_filename,'w')
-    fd.write(ical_string)
-
-
-def write_ical_to_file(ical_data, dest_filename):
-    fd = open(dest_filename,'w')
-    fd.write(ical_data)
+    write_content_to_file(ical_string, dest_filename)
