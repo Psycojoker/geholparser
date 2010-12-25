5b342a67e5fbbbe6fd0984bc30420f17b38b1dff
sevas <f.degroef@gmail.com>
2010-12-25 20:29:59 +0100

    updated sample to fetch data from the internet rather than from the local sample files

--- examples/test_student_calendar.py
+++ examples/test_student_calendar.py
@@ -1,12 +1,13 @@
-#!/usR/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 __author__ = 'Frederic'
 
 import os
 import sys
 sys.path.append("../src")
+from gehol import GeholProxy
 from gehol.studentcalendar import StudentCalendar
-from gehol.converters.icalwriter import convert_student_calendar_to_ical, write_ical_to_file, convert_student_calendar_to_ics_rfc5545
+from gehol.converters.icalwriter import  write_ical_to_file, convert_calendar_to_ics_rfc5545
 from pprint import pprint
 
 DATA_DIR = "../data/student/"
@@ -14,19 +15,33 @@ DATA_FILE = "../data/student/IR_MA1_img_1_14.html"
 DATA_FILE = "../data/student/SC_INFO_MA1_multimedia_1_14.html"
 DATA_FILE = "../data/student/PHILO_BA1_HIST_1_14.html"
 
+first_monday = '20/09/2010'
 
-def make_ical(filename):
+def make_ical_from_local_file(filename):
     f = open(filename)
     cal = StudentCalendar(f)
     print cal.header_data
     pprint(cal.events)
 
-    first_monday = '20/09/2010'
     ical_content = convert_student_calendar_to_ics_rfc5545(cal, first_monday)
     write_ical_to_file(ical_content, "%s.ics" % cal.description)
 
 
-if __name__ == "__main__":
-    for f in os.listdir(DATA_DIR):
-        make_ical(DATA_DIR + f)
+URLs = [("MA1 en sciences informatiques - Spécialisée - Multimedia", "http://164.15.72.157:8080/Reporting/Individual;Student%20Set%20Groups;id;%23SPLUS0FACD0?&template=Ann%E9e%20d%27%E9tude&weeks=1-14&days=1-6&periods=5-33&width=0&height=0"),
+        ("BA1 en sciences de l'ingénieur, orientation ingénieur civil - Série 2B", "http://164.15.72.157:8080/Reporting/Individual;Student%20Set%20Groups;id;%23SPLUSA6299F?&template=Ann%E9e%20d%27%E9tude&weeks=1-14&days=1-6&periods=5-33&width=0&height=0"),
+        ("BA3 en information et communication", "http://164.15.72.157:8080/Reporting/Individual;Student%20Set%20Groups;id;%23SPLUS35F074?&template=Ann%E9e%20d%27%E9tude&weeks=1-14&days=1-6&periods=5-33&width=0&height=0"),
+]
 
+
+def make_ical_from_url(name, url):
+    gehol_proxy = GeholProxy()
+    cal = gehol_proxy.get_student_calendar_from_url(url)
+    ical_data = convert_calendar_to_ics_rfc5545(cal, first_monday)
+    outfile = "%s.ics" % name
+    print "writing ical file : %s" % outfile
+    write_ical_to_file(ical_data, outfile)
+
+
+if __name__ == "__main__":
+    for (profile, url) in URLs:
+        make_ical_from_url(profile, url)
