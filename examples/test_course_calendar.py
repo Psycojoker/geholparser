7d6b447c9f22eb78959b17f0f93625966ca2dfbc
sevas <f.degroef@gmail.com>
2011-09-15 13:06:31 +0200

    temporarily use the new dev server to fetch data

--- examples/test_course_calendar.py
+++ examples/test_course_calendar.py
@@ -5,11 +5,12 @@ from gehol import GeholProxy
 
 
 def test_valid_course(course_mnemo="INFOH500"):
-    gehol_proxy = GeholProxy("164.15.72.157:8080")
+    gehol_proxy = GeholProxy("164.15.72.157:8081")
     print "Fetching events for course : %s" % course_mnemo
     calendar = gehol_proxy.get_course_calendar(course_mnemo)
     print "Result : "
     print calendar
+    print calendar.events
 
 
 def test_nonexistent_course():
